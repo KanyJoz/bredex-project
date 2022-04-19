067b89c1a9ed6b673c55369435a9917577f338e8
Kanyó József <kanyojozsef96@gmail.com>
2022-04-19 12:29:22 +0200

    add dropdown directive

--- formula-one-frontend/src/app/partials/partials.module.ts
+++ formula-one-frontend/src/app/partials/partials.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { DropdownDirective } from './header/dropdown.directive';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HomeComponent,
     FooterComponent,
+    DropdownDirective,
   ],
   imports: [
     SharedModule,
