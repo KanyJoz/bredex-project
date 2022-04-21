c6e315c5cdc18e4c7b89c13750910e6174dc047e
Kanyó József <kanyojozsef96@gmail.com>
2022-04-21 22:33:46 +0200

    submit gom feliratának dinamizálása

--- formula-one-frontend/src/app/racer-teams/racer-team-new-edit/racer-team-new-edit.component.ts
+++ formula-one-frontend/src/app/racer-teams/racer-team-new-edit/racer-team-new-edit.component.ts
@@ -1,15 +1,31 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-racer-team-new-edit',
   templateUrl: './racer-team-new-edit.component.html',
   styleUrls: ['./racer-team-new-edit.component.scss']
 })
-export class RacerTeamNewEditComponent {
+export class RacerTeamNewEditComponent implements OnInit {
     @ViewChild('f') racerTeamForm!: NgForm;
+    inEditMode: boolean = false;
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.route.params.subscribe((params: Params) => {
+            if ('id' in params) {
+                this.inEditMode = true;
+            }
+        });
+    }
 
     onSubmit(): void {
         console.log(this.racerTeamForm);
+    }
+
+    getSubmitButtonLabel(): string {
+        return this.inEditMode ? 'Update Racer Team' : 'Create Racer Team';
     }
 }
